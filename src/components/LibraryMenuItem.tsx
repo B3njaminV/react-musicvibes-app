b68ff3d853b856f0224567ca1e5bef24bb139fff
DJYohann <breuil.yohann@orange.fr>
2023-05-22 09:43:58 +0200

    💄 add first component

---
+++ src/components/LibraryMenuItem.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from "react-native";
+
+export default function LibraryMenuItem() {
+    return (
+        <View style={styles.container}>
+            <Text>Coming soon</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "blue"
+    }
+})
