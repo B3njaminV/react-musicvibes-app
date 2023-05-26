abac904d803ac92a60141320d4d73e52955b7ca6
Benjamin <Benjamin.valleix@gmail.com>
2023-05-26 20:15:14 +0200

    :rotating_light: Avance vue explorer + music item vue

--- src/screens/ExploreScreen.tsx
+++ src/screens/ExploreScreen.tsx
@@ -1,25 +1,89 @@
-import {StyleSheet, Text, View} from "react-native";
+import {FlatList, StyleSheet, Text, View} from "react-native";
 import React from "react";
+import MusicItem from "../components/MusicItem";
 
 export default function ExploreScreen() {
+
+    const musics = [
+        {
+            id: '1',
+            title: 'Music',
+            subtitle: 'Sous-titre',
+            author: 'Auteur',
+            image: require('../assets/podcast.jpg'),
+        },
+        {
+            id: '',
+            title: 'Music ',
+            subtitle: 'Sous-titre ',
+            author: 'Auteur ',
+            image: require('../assets/podcast.jpg'),
+        },
+        {
+            id: '3',
+            title: 'Music ',
+            subtitle: 'Sous-titre ',
+            author: 'Auteur ',
+            image: require('../assets/podcast.jpg'),
+        },
+        {
+            id: '4',
+            title: 'Music ',
+            subtitle: 'Sous-titre ',
+            author: 'Auteur ',
+            image: require('../assets/podcast.jpg'),
+        },
+        {
+            id: '5',
+            title: 'Music ',
+            subtitle: 'Sous-titre ',
+            author: 'Auteur ',
+            image: require('../assets/podcast.jpg'),
+        },
+        {
+            id: '6',
+            title: 'Music',
+            subtitle: 'Sous-titre',
+            author: 'Auteur',
+            image: require('../assets/podcast.jpg'),
+        },
+    ];
+
     return (
         <View style={styles.container}>
-            <View style={styles.centered}>
-                <Text style={styles.title}>Explore Screen</Text>
-            </View>
-            <Text>Mon super texte ...</Text>
+            <Text style={styles.title}>A découvrir</Text>
+            <FlatList
+                data={musics}
+                keyExtractor={(item) => item.id}
+                horizontal
+                renderItem={MusicItem}
+            />
+            <Text style={styles.title}>Pour toi</Text>
+            <FlatList
+                data={musics}
+                keyExtractor={(item) => item.id}
+                horizontal
+                renderItem={MusicItem}
+            />
+            <Text style={styles.title}>Plus ...</Text>
+            <FlatList
+                data={musics}
+                keyExtractor={(item) => item.id}
+                horizontal
+                renderItem={MusicItem}
+            />
         </View>
-    )
+    );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
-    centered: {
-        alignItems: "center"
+        padding: 16,
     },
     title: {
-        fontSize: 20
-    }
+        fontSize: 34,
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
 });
