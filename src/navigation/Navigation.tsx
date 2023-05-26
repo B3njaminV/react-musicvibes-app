448e7c429712470a05c1914746ab3179bb03b6de
DJYohann <breuil.yohann@orange.fr>
2023-05-26 08:19:34 +0200

    :bug: Fix navigation duplication

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -11,14 +11,14 @@ export default function Navigation() {
     return (
         <NavigationContainer>
             <BottomTabNavigator.Navigator initialRouteName="LibraryTab" screenOptions={{ headerShown: false }}>
-                <BottomTabNavigator.Screen name="Explore" component={ExploreNavigator}
+                <BottomTabNavigator.Screen name="ExploreTab" component={ExploreNavigator}
                                            options={{
-                                               title: 'ExploreTab',
+                                               title: 'Explore',
                                                tabBarIcon: ({color}) => <TabBarIcon name="th-large" color={color}/>,
                                            }}/>
-                <BottomTabNavigator.Screen name="Library" component={LibraryNavigator}
+                <BottomTabNavigator.Screen name="LibraryTab" component={LibraryNavigator}
                                            options={{
-                                               title: 'LibraryTab',
+                                               title: 'Library',
                                                tabBarIcon: ({color}) => <TabBarIcon name="music" color={color}/>,
                                            }}/>
                 <BottomTabNavigator.Screen name="SearchTab" component={SearchNavigator}
